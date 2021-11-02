da90e59eb12dff6c1596e3de91e6f94858f9bf55
alimukhtor <james1999007@gmail.com>
2021-11-02 19:05:08 +0100

    added table and pushed values of api

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-window.onload =()=> {
+let loadUser =()=> {
   fetch("https://jsonplaceholder.typicode.com/users")
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data =>{
+    console.log(data)
+    const getBody = document.querySelector(".tbody")
+    getBody.innerHTML = ""
+    const name = data.forEach(element =>
+      getBody.innerHTML += `
+      <tr>
+        <th scope="row">1</th>
+        <td>${element.name}</td>
+        <td>${element.username}</td>
+        <td>${element.email}</td>
+        <td>${element.phone}</td>
+      </tr>
+      `)
+
+  })
 }
+window.onload =()=> {
+  loadUser()
+}
+
+// const dataWithAsync = async () => {
+//
+//         let response = await fetch('url');
+//
+//         let data = await response.json()
+//
+//         return data
+//
+// }
