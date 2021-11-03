a6c2ef35e880322eb0820042809f32efaadf23f8
alimukhtor <james1999007@gmail.com>
2021-11-03 08:17:15 +0100

    added try catch method

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ const tableData =(element)=>`
   <td>${element.phone}</td>
 </tr>`
 let loadUser = async()=> {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const data =  await response.json()
-    console.log(data)
-    const getBody = document.querySelector(".tbody")
-    getBody.innerHTML = ""
-    const name = data.forEach(element =>
-    getBody.innerHTML += tableData(element)
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    if(response.ok){
+      const data =  await response.json()
+      console.log(data);
+      const getBody = document.querySelector(".tbody")
+      getBody.innerHTML = ""
+      const name = data.forEach(element =>
+        getBody.innerHTML += tableData(element)
 
-    )
+      )
+    }else{
+       throw new Error("Users are not fetched!");
+    }
+  } catch (error) {
+    throw error;
+    }
 }
+
 window.onload =()=> {
   loadUser()
 }
